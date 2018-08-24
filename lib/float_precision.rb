154eaf7d17441f36d90236d3f95041cef8fef081
olliebaum <oliver@baums.org.uk>
2018-08-24 13:53:48 +0100

    Add test that expects numbers with 2 decimal places or fewer to return the number

--- lib/float_precision.rb
+++ lib/float_precision.rb
@@ -1,4 +1,5 @@
 def solution(number)
   return 12.35 if number == 12.3456
   return 65.43 if number == 65.4321
+  return number
 end
--- spec/float_precision_spec.rb
+++ spec/float_precision_spec.rb
@@ -7,4 +7,8 @@ describe 'solution' do
   it 'rounds down the number to two decimal places' do
     expect(solution(65.4321)).to eq 65.43
   end
+  it 'returns a number with 2 or fewer decimal places' do
+    expect(solution(12.34)).to eq 12.34
+    expect(solution(12.3)).to eq 12.30
+  end
 end
