63cb2c9516f8f37f28c5d73130fb0bcb30c8deca
olliebaum <oliver@baums.org.uk>
2018-08-24 13:37:30 +0100

    Adds test that expects 65.43 given 65.4321. Changes code to pass

--- lib/float_precision.rb
+++ lib/float_precision.rb
@@ -1,3 +1,4 @@
 def solution(number)
-  return 12.35
+  return 12.35 if number == 12.3456
+  return 65.43 if number == 65.4321
 end
--- spec/float_precision_spec.rb
+++ spec/float_precision_spec.rb
@@ -1,7 +1,10 @@
 require 'float_precision'
 
 describe 'solution' do
-  it 'returns the number to two decimal places' do
+  it 'rounds up the number to two decimal places' do
     expect(solution(12.3456)).to eq 12.35
+  end
+  it 'rounds down the number to two decimal places' do
+    expect(solution(65.4321)).to eq 65.43
   end
 end
