c45723db978d1481c21fb35e77cb716ca4164155
olliebaum <oliver@baums.org.uk>
2018-08-24 14:05:06 +0100

    Add expect condition, expecting 34.57 given 34.567. Refactored float_precision.rb to use .round(2)

--- lib/float_precision.rb
+++ lib/float_precision.rb
@@ -1,5 +1,3 @@
 def solution(number)
-  return 12.35 if number == 12.3456
-  return 65.43 if number == 65.4321
-  return number
+  return number.round(2)
 end
--- spec/float_precision_spec.rb
+++ spec/float_precision_spec.rb
@@ -3,6 +3,7 @@ require 'float_precision'
 describe 'solution' do
   it 'rounds up the number to two decimal places' do
     expect(solution(12.3456)).to eq 12.35
+    expect(solution(34.567)).to eq 34.57
   end
   it 'rounds down the number to two decimal places' do
     expect(solution(65.4321)).to eq 65.43
